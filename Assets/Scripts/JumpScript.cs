7416c36830097715d23c1385d978b20424eb4f83
Hitei <Hirotakatei@gmail.com>
2017-03-10 00:02:40 -0500

    Level Layout finished and Doors working

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+
+    public Vector3 jump;
+    public float jumpForce = 20.0f;
+
+    public bool isGrounded;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0.0f, 20.0f, 0.0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            //Jump Script 
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -46,4 +46,59 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Door")
+        {
+            Debug.Log("WORKKING");
+        }
+
+        if (col.gameObject.tag == "Door" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-19f, -10.55f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door2" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-8.85f, -6.38f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door3" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-13f, -10.55f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door4" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-3.08f, -6.38f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door5" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-7f, -10.55f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door6" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(5.52f, -6.38f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door7" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(4.67f, -2.47f, 0f);
+        }
+
+        if (col.gameObject.tag == "Door8" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(2.41f, -6.39f, 0f);
+        }
+
+        /*if (col.gameObject.tag == "Door3" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.localPosition = new Vector3(-13f, -10.55f, 0f);
+        }
+        */
+        
+    }
+
 }
