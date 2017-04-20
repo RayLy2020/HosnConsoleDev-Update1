d58988e05b4e4134b736d0978a4211b1f4b3ee01
Hitei <Hirotakatei@gmail.com>
2017-04-20 07:24:49 -0400

    please work

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,33 +26,6 @@ public class MovingPlatform : MonoBehaviour
     {
         platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPosition.position, Time.deltaTime * moveSpeed);
 
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MovingPlatform : MonoBehaviour {
-
-    public GameObject platform;
-
-    public float moveSpeed;
-
-    private Transform currentPosition;
-
-    public Transform[] points;
-
-    public int pointSelect;
-
-	// Use this for initialization
-	void Start () {
-        currentPosition = points[pointSelect];
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPosition.position, Time.deltaTime * moveSpeed);
-
->>>>>>> ade574ed77c664f87af85e8ffb53393106fc6a02
         if (platform.transform.position == currentPosition.position)
         {
             pointSelect++;
@@ -63,12 +35,6 @@ public class MovingPlatform : MonoBehaviour {
                 pointSelect = 0;
             }
             currentPosition = points[pointSelect];
-<<<<<<< HEAD
         }
     }
 }
-=======
-        }
-	}
-}
->>>>>>> ade574ed77c664f87af85e8ffb53393106fc6a02
